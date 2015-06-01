9d97b48d59dd9ce90394924dc1adea1306a473fd
kaflan <kaf_90@mail.ru>
2015-06-02 02:33:13 +0300

    normal functional

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 //Ваш код будет здесь
 window.addEventListener('load', function load() {
   var body = document.querySelector('body');
-  var pQuery;
   var buttonQuery;
-  var inputQuery;
   var inputQueryAll;
   var divQuery;
   var divAllQuery;
@@ -15,13 +13,14 @@ window.addEventListener('load', function load() {
   var inputValToNumTwo;
   var text = '+';
   var res;
-  var inputVal;
   var div = document.createElement('div');
   var input = document.createElement('input');
   var button = document.createElement('button');
   var aside = document.createElement('aside');
   var docFrag = document.createDocumentFragment();
+  var nav = document.createElement('nav');
   var p = document.createElement('p');
+  p.innerHTML = 'Это не число';
   var createEL = function () {
     body.appendChild(aside);//беремо боді додаємо асайд.
     aside.appendChild(div);//додаємо в айсід дів
@@ -30,10 +29,8 @@ window.addEventListener('load', function load() {
     div.appendChild(docFrag); //в дів кладемо інпут
     clone = div.cloneNode(true); // clone div  із інпутом
     aside.appendChild(clone);//додаємо в айсід дів
-    docFrag.appendChild(p);
-    divQuery.appendChild(docFrag);
-    pQuery = document.querySelector('p');// вибираємо Р
-    pQuery.textContent = text;
+    divQuery.appendChild(nav);
+    nav.innerHTML = text;
     divAllQuery = document.querySelectorAll('div');// all div to arr and toggle class
     for (i = 0; i < divAllQuery.length; i++) {
       divAllQuery[i].classList.add('error-message');//err mess add to class
@@ -48,13 +45,11 @@ window.addEventListener('load', function load() {
   createEL();
   //після создння елементи найдуться.
   divQuery = document.querySelector('div');
-  pQuery = document.querySelector('p');
   divAllQuery = document.querySelectorAll('div');// all div to arr and toggle class
-  inputQuery = document.querySelector('input');
   inputQueryAll = document.querySelectorAll('input');
   buttonQuery = document.querySelector('button');
   clone = div.cloneNode(true);
-  inputVal = function () {
+  var getInputValues = function () {
     inputValOne = (inputQueryAll[0].value);
     inputValToNumOne = +inputValOne;
     inputValTwo = (inputQueryAll[1].value);
@@ -64,26 +59,38 @@ window.addEventListener('load', function load() {
     return res = inputValToNumOne + inputValToNumTwo;
   };
   valid = function () {
-    inputVal();
-    console.log(inputValToNumOne);
-    if (( isNaN(inputValToNumOne)) || isNaN(inputValToNumTwo) ) {
-      console.log(inputValToNumOne);
-      for (i = 0; i < divAllQuery.length; i++) {
-        divAllQuery[i].classList.add('error-message');
-        divAllQuery[i].appendChild(p);
-        clone = p.cloneNode(true);
-        divAllQuery[0].appendChild(clone);
-        p.innerHTML = 'Это не число';
-      }
-    } else {
-      div = document.createElement('div');
-      body.appendChild(div);
-      divAllQuery = document.querySelectorAll('div');
-      divAllQuery[2].id = 'result';
-      divAllQuery[2].innerHTML = 'Результат: ' + result();
+    getInputValues();
+    var navSelector = document.querySelector('nav');
+    var isValid = true;
+    if (divAllQuery[0].parentNode.contains(p) && divAllQuery[1].parentNode.contains(p)) {
+      p.innerHTML  = '';
     }
+    if (( isNaN(inputValToNumOne))) {
+      divAllQuery[0].classList.add('error-message');
+      divAllQuery[0].appendChild(p);
+      divAllQuery[0].insertBefore(p, navSelector);
+    }
+    if (isNaN(inputValToNumTwo)) {
+      divAllQuery[1].classList.add('error-message');
+      divAllQuery[1].appendChild(p);
+      divAllQuery[1].insertBefore(p, navSelector);
+    }
+    if (!isValid) {
+      return;
+    }
+    divAllQuery[0].classList.remove('error-message');
+    divAllQuery[1].classList.remove('error-message');
+    div = document.createElement('div');
+    body.appendChild(div);
+    divAllQuery = document.querySelectorAll('div');
+    divAllQuery[2].id = 'result';
+    divAllQuery[2].innerHTML = 'Результат: ' + result();
   };
-  inputQuery.addEventListener('keyup', function (event) {
+  inputQueryAll[0].addEventListener('keyup', function (event) {
+    if (event.keyCode !== 13) return;
+    valid();
+  });
+  inputQueryAll[1].addEventListener('keyup', function (event) {
     if (event.keyCode !== 13) return;
     valid();
   });
